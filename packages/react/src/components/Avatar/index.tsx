bd9fbf41d6cddee066db79e828014bc6befa9aae
gabriellafaria <gabisouf@gmail.com>
2023-08-29 17:31:11 -0300

    chore: add the Avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarImage, AvatarFallback } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
