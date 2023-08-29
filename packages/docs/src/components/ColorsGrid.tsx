771bc3cbfa6b332ea995f1819eda108599ddd974
gabriellafaria <gabisouf@gmail.com>
2023-08-28 20:24:28 -0900

    fix: import React

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,16 +1,17 @@
 import { colors } from '@ignite-ui_g/tokens'
 import { getContrast } from 'polished'
+import React from 'react'
 
 export function ColorsGrid() {
   return Object.entries(colors).map(([key, color]) => {
     return (
-      <div key={key} style={{ backgroundColor: color, padding: '2rem'}}>
-        <div 
-          style={{ 
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
             display: 'flex',
             justifyContent: 'space-between',
             fontFamily: 'monospace',
-            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF'
+            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
           }}
         >
           <strong>${key}</strong>
